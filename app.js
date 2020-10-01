afb40e484b5a061526a067012470c827ff5f64f8
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-10-01 16:16:10 -0700

    added katie-refactoring

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import { getRandomThrow, doesUserWin } from './get-random-throw.js';
 
 const clickButton = document.querySelector('#play-button');
-const winsSpan = document.querySelector('#wins-span');
-const lossesSpan = document.querySelector('#losses-span');
-const drawsSpan = document.querySelector('#draws-span');
 const thrownSpan = document.querySelector('#thrown-span');
 const messageSpan = document.querySelector('#message-span');
 const resultsSpan = document.querySelector('#results-span');
 const resetButton = document.querySelector('#reset-button');
 const resetSpan = document.querySelector('#reset-span');
+const concealImage = document.querySelector('#conceal-image');
+// const insertImg = document.getElementById('myImg').src = './assets/scissors.jpeg"';
 
 let wins = 0;
 let losses = 0;
@@ -19,11 +18,11 @@ let resets = 0;
 clickButton.addEventListener('click', () => {
 
     const gameButton = document.querySelector(':checked');
-    const userPicks = gameButton.value;
+    const userPick = gameButton.value;
 
-    const computerChoices = getRandomThrow();
+    const computerChoice = getRandomThrow();
 
-    const finalAnswer = doesUserWin(userPicks, computerChoices);
+    const finalAnswer = doesUserWin(userPick, computerChoice);
     messageSpan.textContent = finalAnswer;
 
     if (finalAnswer === 'You win!') {
@@ -34,8 +33,21 @@ clickButton.addEventListener('click', () => {
         draws++;
     }
 
-    thrownSpan.textContent = `Computer chose: ${computerChoices}.`;
+    document.getElementById('my-img-paper').src = '';
+    document.getElementById('my-img-rock').src = '';
+    document.getElementById('my-img-scissors').src = '';
+
+    thrownSpan.textContent = `Computer chose: ${computerChoice}.`;
+    if (computerChoice === 'scissors') {
+        document.getElementById('my-img-scissors').src = './assets/scissors.jpeg';
+    } else if (computerChoice === 'rock') {
+        document.getElementById('my-img-rock').src = './assets/rock.jpeg';
+    } else if (computerChoice === 'paper') {
+        document.getElementById('my-img-paper').src = './assets/paper.jpeg';
+    }
+
     resultsSpan.textContent = `Wins: ${wins}; Losses ${losses}; Draws: ${draws}`;
+    // concealImage.style.display = 'block';
 });
 // ***********END OF PLAY BUTTON**************
 
@@ -47,5 +59,11 @@ resetButton.addEventListener('click', () => {
     resets ++;
     resultsSpan.textContent = `Wins: ${wins}; Losses ${losses}; Draws: ${draws}`;
     resetSpan.textContent = resets;
-})
+    messageSpan.textContent = '';
+    thrownSpan.textContent = '';
+    // concealImage.style.display = 'none';
+    document.getElementById('my-img-paper').src = '';
+    document.getElementById('my-img-rock').src = '';
+    document.getElementById('my-img-scissors').src = '';
+});
 // ***********END OF RESET BUTTON***********
--- get-random-throw.js
+++ get-random-throw.js
@@ -8,27 +8,15 @@ export function getRandomThrow() {
     } else if (randomThrow === 3) {
         return 'scissors';
     }
-};
+}
 
 export function doesUserWin(player, computer) {
 
-    if (player === 'rock' && computer === 'rock') {
-        return 'Draw!'
-    } else if  (player === 'rock' && computer === 'paper') {
-        return 'You lose.'
-    } else if  (player === 'rock' && computer === 'scissors') {
-        return 'You win!'
-    } else if (player === 'paper' && computer === 'paper') {
-        return 'Draw!'
-    } else if  (player === 'paper' && computer === 'scissors') {
-        return 'You lose.'
-    } else if  (player === 'paper' && computer === 'rock') {
-        return 'You win!'
-    } else if (player === 'scissors' && computer === 'scissors') {
-        return 'Draw!'
-    } else if  (player === 'scissors' && computer === 'rock') {
-        return 'You lose.'
-    } else if  (player === 'scissors' && computer === 'paper') {
-        return 'You win!'
+    if (player === 'rock' && computer === 'rock' || player === 'paper' && computer === 'paper' || player === 'scissors' && computer === 'scissors') { 
+        return 'Draw!';
+    } else if (player === 'rock' && computer === 'paper' || player === 'paper' && computer === 'scissors' || player === 'scissors' && computer === 'rock') {
+        return 'You lose.';
+    } else if (player === 'rock' && computer === 'scissors' || player === 'paper' && computer === 'rock' || player === 'scissors' && computer === 'paper') {
+        return 'You win!';
     }
-};
+}
